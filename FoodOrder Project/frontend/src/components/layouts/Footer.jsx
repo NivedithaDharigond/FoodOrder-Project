72ced43c9de05dd97328bce3aef06094e18015c5
NivedithaDharigond <nivedithadharigond@gmail.com>
2024-12-21 18:29:45 +0530

    initial commit

---
+++ FoodOrder Project/frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <>
+            <footer>
+                <p className='text-center'>
+                    Food Delivery website -2024. All Rights reserved by Niveditha
+                </p>
+
+            </footer>
+        </>
+    </div>
+  )
+}
